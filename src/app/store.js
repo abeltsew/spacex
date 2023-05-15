6c4de1383f8c27869f4ad48163a390af39d7da45
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-05-15 14:02:15 +0300

    Fetch Mission from API

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionReducer from '../feature/mission/missionSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    mission: missionReducer,
+  },
 });
 
 export default store;
