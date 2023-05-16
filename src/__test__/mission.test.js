d02115a61d3e0d3f9cf069ef7880a237f28f07bf
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-05-16 11:25:46 +0300

    Add Render Mission test

--- src/__test__/mission.test.js
+++ src/__test__/mission.test.js
@@ -1,18 +1,32 @@
 import renderer from 'react-test-renderer';
-import Mission from '../components/Mission';
 import { Provider } from 'react-redux';
-import '@testing-library/jest-dom/extend-expect';
 import configureStore from 'redux-mock-store';
-import { initialState } from '../feature/mission/missionSlice';
 import { render, screen } from '@testing-library/react';
+import Mission from '../components/Mission';
 import store from '../app/store';
+import { initialState } from '../feature/mission/missionSlice';
+
+const data = {
+  missions: [
+    {
+      mission_id: '1',
+      mission_name: 'Falcon 1',
+      description: 'The falcon 1 description',
+    },
+    {
+      mission_id: '2',
+      mission_name: 'Falcon 2',
+      description: 'The falcon 2 description',
+    },
+  ],
+};
 
 it('should render the mission page', () => {
   const tree = renderer
     .create(
       <Provider store={store}>
         <Mission />
-      </Provider>
+      </Provider>,
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
@@ -20,13 +34,71 @@ it('should render the mission page', () => {
 
 it('should render the mission', () => {
   const mockStore = configureStore();
+  const missionStore = mockStore(initialState);
 
-  render(
-    <Provider store={mockStore(initialState)}>
-      <Mission />
-    </Provider>
-  );
+  missionStore.clearActions();
+  const getMissions = () => ({ type: 'GET_MISSIONS' });
 
-  const loading = screen.getByText(/Loading../i);
-  expect(loading).toBeInTheDocument();
+  missionStore.dispatch(getMissions());
+
+  const actions = missionStore.getActions();
+  const expectedPayload = { type: 'GET_MISSIONS' };
+  expect(actions).toEqual([expectedPayload]);
+});
+
+it('should render the initial state Properly', () => {
+  const mockStore = configureStore();
+  const missionStore = mockStore(initialState);
+
+  expect(initialState).toEqual(missionStore.getState());
+});
+
+it('should render the Mission state Properly', () => {
+  const mockStore = configureStore();
+
+  const missionStore = mockStore(data);
+
+  expect(data).toEqual(missionStore.getState());
+});
+
+describe('Test Mission Rendering to the screen', () => {
+  const mockStore = configureStore();
+
+  it('should render the mission list to the screen', () => {
+    const newState = {
+      mission: {
+        missions: [
+          {
+            mission_id: 'id-1',
+            mission_name: 'Falcon 1',
+            description: 'Falcon 1 description',
+            reserved: false,
+          },
+          {
+            mission_id: 'id-2',
+            mission_name: 'Falcon 2',
+            description: 'Falcon 2 description',
+            reserved: false,
+          },
+        ],
+      },
+    };
+    const store = mockStore(newState);
+    store.dispatch({
+      type: 'get/mission',
+      payload: newState,
+    });
+
+    render(
+      <Provider store={store}>
+        <Mission />
+      </Provider>,
+    );
+
+    const falcon1 = screen.getByText('Falcon 1');
+    const falcon2 = screen.getByText('Falcon 2');
+
+    expect(falcon1).toBeInTheDocument();
+    expect(falcon2).toBeInTheDocument();
+  });
 });
