36c11f1d5ae0b8c6ea5f1b97fb6460a59c32c440
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-05-17 09:39:16 +0300

    Fix Logger Issue

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,14 @@
-import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
+import { configureStore } from '@reduxjs/toolkit';
+import { logger } from 'redux-logger';
 import missionReducer from '../feature/mission/missionSlice';
 import rocketReducer from '../feature/rocket/rocketSlice';
 
-const store = configureStore(
-  {
-    reducer: {
-      mission: missionReducer,
-      rocket: rocketReducer,
-    },
+const store = configureStore({
+  reducer: {
+    mission: missionReducer,
+    rocket: rocketReducer,
   },
-  applyMiddleware(logger),
-);
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
 export default store;
